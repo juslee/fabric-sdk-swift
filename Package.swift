7d77a5ad1f131f33ee5b131f92fda338f82b2fcf
Justin Lee <triplez@justinlee.sg>
2017-02-22 03:22:50 +0800

    Add grpc-swift dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "fabric-sdk-swift",
     dependencies: [
-        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,27))
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,27)),
+        .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,1,8))
     ]
 )
