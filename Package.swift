0d4f9e2deaa17442017888008aba7252685fc17e
Justin Lee <triplez@justinlee.sg>
2017-03-20 16:54:47 +0800

    Update the proto changes

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
            dependencies: ["Protos"])
     ],
     dependencies: [
-        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,27)),
+        .Package(url: "https://github.com/apple/swift-protobuf.git", Version(0,9,28)),
         .Package(url: "https://github.com/grpc/grpc-swift.git", Version(0,1,10))
     ]
 )
